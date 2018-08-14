6c7754e015f572211893a3d902438ae295c4bc10
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 01:35:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function compareHometown(driver, location) {
 // log each driver name whose home matches location
 function logDriversByHometown(drivers, location) {
   let filtered_drivers = drivers.filter(compareHometown(driver, location));
+  debugger;
   filtered_drivers.forEach(function(driver) {
     console.log(driver.name);
   })
