13cf5b20d6ae4cb8d33b20eb2e8cfea5f7d87056
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 03:07:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function driversByRevenue(drivers) {
 }
 
 function driversByName(drivers) {
-  return drivers.sort(function(a,b){
+  return drivers.slice(0).sort(function(a,b){
     return a.name.localeCompare(b.name);
   })
 }
