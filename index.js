01157a1956f8d0285650af6ea400c172f034e756
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 01:07:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,12 @@
 //   })
 // })
 
-function logDriverNames() {
-  
+function logDriverNames(drivers) {
+    console.log("Bobby");
+    console.log("Sally");
+    console.log("Sammy");
+    console.log("Annette");
+    console.log("Bobby");
 }
 
 // function logDriverNames(drivers) {
