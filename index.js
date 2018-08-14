71e0bc868cd78d8986e185626e867333e98dc5e4
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 03:49:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function driversByName(drivers) {
 }
 
 function totalRevenue(drivers) {
-  return drivers.reduce((a, b) => ( {revenue: a.revenue + b.revenue} ), 0 );
+  return drivers.reduce((a, b) => ( {revenue: a.revenue + b.revenue} ) );
 //   return drivers.reduce(function(total, driver) {
 //     return total + driver.revenue}, 0 )
 }
