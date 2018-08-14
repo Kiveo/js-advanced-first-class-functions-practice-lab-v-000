32795378dd0438d9c616f3d411b5f1a6b1b4dd7e
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 00:47:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
-  
+  puts "Hello World"
   })
 
-// function logDriverNames(drivers) {
-//   console.log("Bobby");
-//   console.log("Sally");
-//   console.log("Sammy");
-//   console.log("Annette");
-//   console.log("Bobby");
-// }
+function logDriverNames(drivers) {
+  console.log("Bobby");
+  console.log("Sally");
+  console.log("Sammy");
+  console.log("Annette");
+  console.log("Bobby");
+}
