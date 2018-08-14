f25a86d3b6ca62ec937560ff7eaa9064f0030aa3
Maverick Tesini <mavericktesini@gmail.com>
2018-08-14 01:08:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 // Code your solution in this file!
-// function logDriverNames(drivers) {
-//   drivers.forEach(function(driver){
-//     debugger;
-//     console.log(driver.name);
-//   })
-// })
 
 function logDriverNames(drivers) {
-  
-}
+  drivers.forEach(function(driver){
+    debugger;
+    console.log(driver.name);
+  })
+})
+
 
 // function logDriverNames(drivers) {
 //   console.log("Bobby");
